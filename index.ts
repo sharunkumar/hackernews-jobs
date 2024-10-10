3d814156129972f37114aa5bbb93397351989112
Sharun <sharunksplus@gmail.com>
2024-10-09 22:02:19 -0400

    implement getting jobs from hackernews api

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const jobstories = await fetch('https://hacker-news.firebaseio.com/v0/jobstories.json')
+
+const jobstoriesdata = await Promise.all((await jobstories.json() as number[]).map(async (job) => {
+  const jobdata = await fetch(`https://hacker-news.firebaseio.com/v0/item/${job}.json`)
+  return jobdata.json()
+}))
+
+console.log(jobstoriesdata)
