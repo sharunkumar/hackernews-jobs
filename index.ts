59b3b87e42715c956e3040a8754b2087a67fd5a6
Sharun <sharunksplus@gmail.com>
2024-10-09 22:41:19 -0400

    install biome, update workspace extensions

--- index.ts
+++ index.ts
@@ -1,30 +1,35 @@
-import { readFileSync, writeFileSync } from 'fs';
+const jobstories = await fetch(
+	"https://hacker-news.firebaseio.com/v0/jobstories.json",
+);
 
-const jobstories = await fetch('https://hacker-news.firebaseio.com/v0/jobstories.json')
-
-const jobstoriesdata = await Promise.all((await jobstories.json() as number[]).map(async (job) => {
-  const jobdata = await fetch(`https://hacker-news.firebaseio.com/v0/item/${job}.json`)
-  return jobdata.json()
-}))
+const jobstoriesdata = await Promise.all(
+	((await jobstories.json()) as number[]).map(async (job) => {
+		const jobdata = await fetch(
+			`https://hacker-news.firebaseio.com/v0/item/${job}.json`,
+		);
+		return jobdata.json();
+	}),
+);
 
 const markdownContent = jobstoriesdata.reduce((markdown, job) => {
-  if (job.title && job.url) {
-    return markdown + `| ${job.title} | [Apply](${job.url}) |\n`;
-  }
-  return markdown;
-}, '| Title | Apply |\n|-------|-----|\n');
+	if (job.title && job.url) {
+		return `${markdown}| ${job.title} | [Apply](${job.url}) |\n`;
+	}
+	return markdown;
+}, "| Title | Apply |\n|-------|-----|\n");
 
-// Read the existing README.md file
-const readmePath = 'README.md';
-let readmeContent = readFileSync(readmePath, 'utf-8');
+const readmeFile = Bun.file("README.md");
+const readmeContent = await readmeFile.text();
 
 // Replace the content between the comments
-const startComment = '<!-- table start -->';
-const endComment = '<!-- table end -->';
+const startComment = "<!-- table start -->";
+const endComment = "<!-- table end -->";
 const regex = new RegExp(`${startComment}[\\s\\S]*${endComment}`);
-const updatedContent = readmeContent.replace(regex, `${startComment}\n\n${markdownContent}\n${endComment}`);
+const updatedContent = readmeContent.replace(
+	regex,
+	`${startComment}\n\n${markdownContent}\n${endComment}`,
+);
 
-// Write the updated content back to README.md
-writeFileSync(readmePath, updatedContent);
+readmeFile.writer().write(updatedContent);
 
-console.log('README.md has been updated with the latest job stories.');
+console.log("README.md has been updated with the latest job stories.");
